5271272f43b6b40b19376aac7fa697d0b0c1abdb
Eloi Frias <eloifp49@gmail.com>
2025-04-09 17:47:33 +0200

    contact section complet + navbar responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+// nav bar menu 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".nav-menu");
+    const navMenu = document.querySelector(".nav-links ul");
+    const navLinks = navMenu.querySelectorAll("a");
+  
+    // Mostrar menú al hacer clic en el botón
+    menuButton.addEventListener("click", () => {
+      navMenu.classList.add("active");
+    });
+  
+    // Ocultar menú cuando el cursor sale del área
+    navMenu.addEventListener("mouseleave", () => {
+      navMenu.classList.remove("active");
+    });
+  
+    // Ocultar menú al hacer clic en un enlace
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+      });
+    });
+  });
+  
 //Slider "about-me"
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -27,3 +52,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+//copy email "contact me"
+  
+function copyEmail() {
+    const email = document.getElementById("email").textContent;
+    const msg = document.getElementById("copy-msg");
+  
+    navigator.clipboard.writeText(email)
+      .then(() => {
+        msg.textContent = "Copied!";
+        setTimeout(() => {
+          msg.textContent = "";
+        }, 1000); // El mensaje desaparece después de 2 segundos
+      })
+      .catch((err) => {
+        console.error("Failed to copy email: ", err);
+        msg.textContent = "Failed to copy";
+      });
+  }
+  
